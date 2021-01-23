56f7f210451a23d808226fd12c0e7e79f2a2c379
suzukitoshinari <ay658mymt@gmail.com>
2021-01-23 23:00:31 +0900

    commit

---
+++ src/AllPokemon.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import PokemonCard from './PokemonCard'
+
+const useStyles = makeStyles({
+    container: {
+        backgroundColor: 'coral',
+        height: '100vh'
+    },
+    head: {
+        textAlign: 'center',
+        backgroundColor: 'green',
+    }
+});
+
+function AllPokemon() {
+    const classes = useStyles();
+
+    return (
+        <>
+            <Container fixed>
+                <Typography className={classes.container} component='div'>
+                    <h1 className={classes.head}>Pokemon API</h1>
+                    <PokemonCard />
+                </Typography>
+            </Container>
+        </>
+    )
+}
+
+export default AllPokemon;
